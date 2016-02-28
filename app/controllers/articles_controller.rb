10c881c4c0068cbba6b589d9b8724db123883d8a
Huy T <td.huycan@gmail.com>
2016-02-28 22:13:15 +0700

    Add search

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,12 @@ class ArticlesController < ApplicationController
   # GET /articles
   # GET /articles.json
   def index
-    @articles = Article.all
+    if params[:search]
+      @articles = Article.search(params[:search]).order(created_at: :desc)
+    else
+      @articles = Article.order(created_at: :desc).all
+    end
+
     @markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
   end
 
